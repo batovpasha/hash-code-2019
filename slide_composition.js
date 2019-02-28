ef816938d1b860cce5ec0c7b97b82381a1122efe
CodeGangsta44 <dovroman44@gmail.com>
2019-02-28 21:44:19 +0200

    Slide-composer added

---
+++ slide_composition.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function delta(){};
+
+function slide_composition(slides_array){
+  let result = [].push(slides_array.pop());
+
+  while(slides_array.length != 0){
+    let best_index = 0;
+    let best_slide = delta(result[result.length - 1], slides_array[0]);
+
+    for (let i = 0; i < slides_array.length; i++){
+      let curr_slide = delta(result[result.length - 1], slides_array[i]);
+
+      if (curr_slide.delta < best_slide.delta) {
+        best_slide = curr_slide;
+        best_index = i;
+
+      } else if (curr_slide.delta === best_slide.delta && curr_slide.interes > best_slide.interes) {
+        best_slide = curr_slide;
+        best_index = i;
+      }
+    }
+    result.push(best_slide);
+    slides_array.splice(best_index, 1);
+  }
+}
